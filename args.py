91e9dd1bfca9692e678d93882cc45cd2970023d9
SivaSankarS365 <sivasankars365@gmail.com>
2023-01-31 16:30:39 +0530

    restructring

--- args.py
+++ args.py
@@ -1,31 +1,31 @@
 from imports import *
 # to do handling Question generation for BM25
-# optimization
 # ranker - crossencoder + kimcnn
 
 
 @dataclass
-class Retriever:
+class RetrieverArgs:
     ensemble = ['bm25', 'dpr', 'colbert']
     
     k1: float = 3
     b: float = 0.75
     sparse_top_k: int = 100
     
+    dpr_batch_size: int = 32
     dpr_model_Q: str = 'flax-sentence-embeddings/multi-QA_v1-mpnet-asymmetric-Q'
     dpr_model_P: str = 'flax-sentence-embeddings/multi-QA_v1-mpnet-asymmetric-A'
     dpr_top_k: int = 100
     
     colbert_batch_size: int = 4
-    colbert_para_maxlength: int = 300 # If increased will cost more memory
-    colbert_qn_maxlength: int = 40 # If increased, increases performance with higher latency
+    colbert_para_maxlength: int = 300
+    colbert_qn_maxlength: int = 40
     colbert_top_k: int = 100
     
     weights = (['bm25', 0.33], ['dpr', 0.33, ], ['colbert', 0.33])
     voting_top_k: int = 5
     
 @dataclass
-class Reader:
+class ReaderArgs:
     onnx: bool = False  # onnx cpu only
     model_name: str = "deepset/tinyroberta-squad2"
     batch_size: int = 4
@@ -35,9 +35,10 @@ class Reader:
 @dataclass
 class TrainingArgs:
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    num_workers: int = os.cpu_count()
 
-    retriever = Retriever()
-    reader = Reader()
+    retriever = RetrieverArgs()
+    reader = ReaderArgs()
     
     root_dir: str = './'
     checkpoints_dir: str = f'{root_dir}/checkpoints/'
--- models/retrievers/dpr.py
+++ models/retrievers/dpr.py
@@ -12,19 +12,24 @@ class DenseRetriever():
         self.fit(df)
     
     def fit(self, df):
+        batch_size = self.args.retriever.dpr_batch_size
         df = copy.deepcopy(df)
+        if self.model_P is None:
+            self.model_P = SentenceTransformer(self.args.retriever.dpr_model_P).to(self.args.device)
         cols = ['Para_id', 'Paragraph', 'Theme']
         assert df.columns.isin(cols).sum()==3 , "Verify Para_id, Paragraph, Theme are in the columns"
         
         paras = df.loc[:,cols].drop_duplicates().reset_index(drop=True)
         self.idx_to_para_id = dict(zip(paras.index, paras.Para_id))
         paras.loc[:,'Paragraph'] = paras.Paragraph.apply(lambda x: clean_text(x, stem=False))
-        self.embeddings = self.model_P.encode(paras.Paragraph,show_progress_bar=True)
+        self.embeddings = self.model_P.encode(paras.Paragraph, batch_size=batch_size, show_progress_bar=True)
         self.theme_to_idxs = paras.index.to_series().groupby(paras.Theme).unique()
+        self.model_P = None
         
     def predict(self, questions, theme):
+        batch_size = self.args.retriever.dpr_batch_size
         questions = [clean_text(q, stem=False) for q in questions]
-        dense_qns = self.model_Q.encode(questions, show_progress_bar=False)
+        dense_qns = self.model_Q.encode(questions, batch_size=batch_size, show_progress_bar=False)
         idx = self.theme_to_idxs[theme].tolist()
         top_k = self.args.retriever.dpr_top_k
         
@@ -37,6 +42,7 @@ class DenseRetriever():
     def to(self, device):
         self.args.device = device
         self.model_Q.to(device)
-        self.model_P.to(device)
         self.model_Q._target_device = device
-        self.model_P._target_device = device
+        if self.model_P is not None:
+            self.model_P.to(device)
+            self.model_P._target_device = device
